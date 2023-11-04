e6f155bde57f250e45112ccdedb2e7d1702fb44a
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-11-03 21:48:37 -0400

    Fixed webpack.config build to bundle assets properly

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -14,18 +11,38 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
+      path: path.resolve(__dirname, 'dist'),
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, '/'),
+      publicPath: './'
     },
-
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+        favicon: './favicon.ico',
+        inject: 'head',
+      }),
+      new WebpackPwaManifest({
+        name: 'Offline Text Editor',
+        short_name: 'Offline Text Editor',
+        description: 'Offline Text Editor!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials',
+        icons: [
+          {
+            src: path.resolve(__dirname, 'src/images/android-chrome-512x512.png'),
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js'
+      })
+    ],
     module: {
       rules: [
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.js$/,
+          test: /\.?js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
@@ -34,24 +51,15 @@ module.exports = () => {
             }
           }
         },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'),
-        filename: 'index.html'
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js'
-      }),
-      new WebpackPwaManifest({
-        name: 'Offline Text Editor',
-        short_name: 'Offline Text Editor',
-        description: 'Offline Text Editor!',
-        background_color: '#ffffff',
-        crossorigin: 'use-credentials'
-      }),
-    ],
   };
 };
