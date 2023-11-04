ba801d8224c6a353ef69594175161ee5ee7b9d13
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-11-03 22:21:39 -0400

    Fixed pwa install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,42 +1,24 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let deferredPrompt;
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
   event.preventDefault();
-  
-  // Stash the event so it can be triggered later.
-  let deferredPrompt = event;
-  
-  // Update UI notify the user they can add to home screen
-  butInstall.style.display = 'block';
+  deferredPrompt = event;
+  butInstall.style.display = 'inline-block';
+});
 
-  butInstall.addEventListener('click', async () => {
-    // hide our user interface that shows our A2HS button
-    butInstall.style.display = 'none';
-    
-    // Show the prompt
-    deferredPrompt.prompt();
-    
-    // Wait for the user to respond to the prompt
-    const choiceResult = await deferredPrompt.userChoice;
-    
+butInstall.addEventListener('click', () => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
     if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
+      console.log('User accepted the installation');
     } else {
-      console.log('User dismissed the A2HS prompt');
+      console.log('User declined the installation');
     }
-    
     deferredPrompt = null;
   });
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-// This has been handled within the 'beforeinstallprompt' event listener above.
-
-// TODO: Add a handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  // Log app installed to the console
-  console.log('PWA was installed');
+  console.log('app installed')
 });
